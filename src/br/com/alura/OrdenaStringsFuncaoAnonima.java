1cd65d57769bad865534169c87087d84e1078043
Adriano Avelino <adriano-st777@hotmail.com>
2021-01-17 14:18:16 -0300

    renomeia classe OrdenaString

--- src/br/com/alura/OrdenaStringsFuncaoAnonima.java
+++ src/br/com/alura/OrdenaStringsFuncaoAnonima.java
@@ -6,7 +6,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.function.Consumer;
 
-public class OrdenaStrings {
+public class OrdenaStringsFuncaoAnonima {
 
 	public static void main(String[] args) {
 		List<String> palavras = new ArrayList<>();
