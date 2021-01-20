63a8140aa3694c2bd52ac714314a9643067bf462
Adriano Avelino <adriano-st777@hotmail.com>
2021-01-19 21:07:22 -0300

    adiciona exemplo de uso com stream

---
+++ src/br/com/alura/Stream.java
@@ -0,0 +1,43 @@
+package br.com.alura;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Stream {
+	public static void main(String[] args) {
+		List<Curso> cursos = new ArrayList<>();
+		cursos.add(new Curso("Python", 45));
+		cursos.add(new Curso("JavaScript", 150));
+		cursos.add(new Curso("Java 8", 113));
+		cursos.add(new Curso("C", 55));
+		
+		cursos.stream()
+		   .filter(c -> c.getAlunos() > 100)
+		   .map(Curso::getAlunos)
+		   .forEach(System.out::println);
+		
+		int soma = cursos.stream()
+				   .filter(c -> c.getAlunos() > 100)
+				   .mapToInt(Curso::getAlunos)
+				   .sum();
+		System.out.println(soma);
+	}
+}
+
+class Curso {
+    private String nome;
+    private int alunos;
+
+    public Curso(String nome, int alunos) {
+        this.nome = nome;
+        this.alunos = alunos;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getAlunos() {
+        return alunos;
+    }
+}
